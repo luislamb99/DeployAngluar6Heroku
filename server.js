bb4ae2afb069e7370bdcbba27dacfeb92a0e5d06
Luis MB <ingluismb@gmail.com>
2018-10-23 00:37:24 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,14 @@
+const  express =  required('express'),
+  path = required('path');
+
+const app = express();
+
+app.use(express.static('./dist/myapp'));
+
+app.get('/*', (req, res) => {
+  res.sendFile( path.join(__dirname, '/dist/myapp/index.html') );
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server Started');
+});
