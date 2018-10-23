4a33b574af06a62b988388264ce5b479d91a332b
Luis MB <ingluismb@gmail.com>
2018-10-23 01:50:25 -0500

    make it better

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const  express =  required('express'),
-  path = required('path');
+//Install express server
+const express = require('express');
+
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('/dist/myapp'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/myapp'));
 
-app.get('/*', (req, res) => {
-  res.sendFile( path.join(__dirname, '/dist/myapp/index.html') );
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/myapp/index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log('Server Started');
-})
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
